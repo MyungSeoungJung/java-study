17c32e7b353c04c523700703f4b8f96e6b1aeb66
msj <audtmdwjd@naver.com>
2023-06-28 16:18:58 +0900

    내가 바꿨따

--- src/ch11_thread/MyService.java
+++ src/ch11_thread/MyService.java
@@ -10,6 +10,9 @@ public class MyService extends Thread{
     //Thread 클래스를 상속받고, run메서드를 구현
     //run메서드가 실행될때 추가적인 스레드가 생성
     //샘플에서는 60회 출력하고, 1회(1000ms)마다 멈춤
+    ///wefwefwefwefwefefwefwefwefwfewef
+    //wefwefwwefwefwefewf
+
     @Override
     public void run(){
         for (int i = 0; i <= (int)(Math.random() * 1000); i++) {
